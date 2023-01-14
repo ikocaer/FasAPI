c56e77a2793b6e14a5695bc3b990939ee0c1b560
ikocaer <76222783+ikocaer@users.noreply.github.com>
2023-01-14 23:06:48 +0300

    Optional2

--- books2.py
+++ books2.py
@@ -5,20 +5,37 @@ from uuid import UUID
 
 app = FastAPI()
 
+
 class Book(BaseModel):
     id: UUID
     title: str = Field(min_lenght=1)
-    author: str
+    author: str = Field(min_length=1, max_length=100)
     description: Optional[str] = Field(title="Description of the book",
-                             max_length=100,
-                             min_length=1)
-    rating: int
+                                       max_length=100,
+                                       min_length=1)
+    rating: int = Field(gt=-1, lt=101)  # En düşük -1 en yüksek 101 arasında
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "856c3b74-c254-4d31-ac2f-7a9ccb8f0b74",
+                "title": "Computer Science Pro",
+                "author": "Codingwithroby",
+                "description": "Description",
+                "rating": 75
+            }
+        }
+
 
 BOOKS = []
 
+
 @app.get("/")
 async def read_all_books():
+    if len(BOOKS) < 1:
+        create_books_no_api()
     return BOOKS
+
 
 @app.post("/")
 async def create_book(book: Book):
@@ -26,15 +43,28 @@ async def create_book(book: Book):
     return book
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+def create_books_no_api():
+    book_1 = Book(id="806c3b74-c254-4d31-ac2f-7a9ccb8f0b74",
+                  title="Title 1",
+                  author="Author 1",
+                  description="Description 1",
+                  rating=60)
+    book_2 = Book(id="816c3b74-c254-4d31-ac2f-7a9ccb8f0b74",
+                  title="Title 2",
+                  author="Author 2",
+                  description="Description 2",
+                  rating=70)
+    book_3 = Book(id="826c3b74-c254-4d31-ac2f-7a9ccb8f0b74",
+                  title="Title 3",
+                  author="Author 3",
+                  description="Description 3",
+                  rating=80)
+    book_4 = Book(id="836c3b74-c254-4d31-ac2f-7a9ccb8f0b74",
+                  title="Title 4",
+                  author="Author 4",
+                  description="Description 4",
+                  rating=90)
+    BOOKS.append(book_1)
+    BOOKS.append(book_2)
+    BOOKS.append(book_3)
+    BOOKS.append(book_4)
