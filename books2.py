2eeab7eb6804d8b4a54301f58820bd3bb666a54c
ikocaer <76222783+ikocaer@users.noreply.github.com>
2023-01-15 00:16:07 +0300

    Optional3

--- books2.py
+++ books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from uuid import UUID
 
@@ -22,25 +22,55 @@ class Book(BaseModel):
                 "title": "Computer Science Pro",
                 "author": "Codingwithroby",
                 "description": "Description",
-                "rating": 75
+                "rating": 80
             }
         }
-
 
 BOOKS = []
 
 
 @app.get("/")
-async def read_all_books():
+async def read_all_books(books_to_return: Optional[int] = None):
     if len(BOOKS) < 1:
         create_books_no_api()
+    if books_to_return and len(BOOKS) >= books_to_return > 0:
+        i = 1
+        new_books = []
+        while i <= books_to_return:
+            new_books.append(BOOKS[i-1])
+            i+=1
+        return new_books
     return BOOKS
 
+@app.get("/book/{book_id}")
+async def read_book(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+
+@app.post("/{book_id}")
+async def update_book(book_id: UUID, book:Book):
+    counter = 0
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            BOOKS[counter -1] = book
+            return book
 
 @app.post("/")
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
+
+@app.delete("/{boook_id}")
+async def delete_book(book_id: UUID):
+    counter = 0
+    for x in BOOKS:
+        counter += 1
+        if x.id == book_id:
+            del BOOKS[counter-1]
+            return f"ID: {book_id} deleted"
+    raise HTTPException(status_code=404, detail="Book not found")
 
 
 def create_books_no_api():
