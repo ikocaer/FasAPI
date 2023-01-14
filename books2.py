95394b21e862562bb6c159b9cf668587ebc40f33
ikocaer <76222783+ikocaer@users.noreply.github.com>
2023-01-14 13:20:25 +0300

    first commit

---
+++ books2.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_lenght = 1)
+    author: str
+    description: str
+    rating: int
+
+BOOKS = []
+
+@app.get("/")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/")
+async def create_book(book: Book):
+    BOOKS.append(book)
+    return book
+
+
+
+
+
+
+
+
+
+
+
+
+
+
